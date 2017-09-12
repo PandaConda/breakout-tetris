b19a76424755a0b1050933ff434f0f808345e368
August Almgren <august.almgren@gmail.com>
2017-09-12 14:49:23 +0200

    Implemented Bouncing
    The ball now shoots off either to the left or to the right, and then bounces.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		const int thrust = 1;
+		if (Random.Range(0, 2) == 0) {
+			body.AddForce(transform.up + transform.right * thrust);
+		} else {
+			body.AddForce(transform.up - transform.right * thrust);
+
+		}
+	}
+}
