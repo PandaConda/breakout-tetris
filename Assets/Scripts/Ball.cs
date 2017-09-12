faffe019b772fd175eef2c6ff919c0f790cafda1
August Almgren <august.almgren@gmail.com>
2017-09-12 15:46:14 +0200

    Ball respawns when it gets to bottom
    Respawns the ball in its original position when it hits the bottom of the
    map.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,13 +8,23 @@ public class Ball : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log("ball start");
 		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		body.position = new Vector2(0, -525);
+		body.velocity = Vector2.zero;
+		//body.MovePosition(new Vector2(0, -525));
 		const int thrust = 1;
 		if (Random.Range(0, 2) == 0) {
 			body.AddForce(transform.up + transform.right * thrust);
 		} else {
 			body.AddForce(transform.up - transform.right * thrust);
 
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.gameObject.name == "Bottom Wall") {
+			Start();
 		}
 	}
 }
