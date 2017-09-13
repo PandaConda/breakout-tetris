093dd61b8fa7d4e19d3320893a25c07c0c0c2d2f
August Almgren <august.almgren@gmail.com>
2017-09-13 14:21:06 +0200

    Paddle collides with sides
    Adds collision detection to the paddle for the left and right sides of the
    map.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	private const int THRUST = 1;
+
 	public Rigidbody2D body;
 
-	// Use this for initialization
 	void Start () {
-		Debug.Log("ball start");
 		Rigidbody2D body = GetComponent<Rigidbody2D>();
 		body.position = new Vector2(0, -525);
 		body.velocity = Vector2.zero;
-		//body.MovePosition(new Vector2(0, -525));
-		const int thrust = 1;
 		if (Random.Range(0, 2) == 0) {
-			body.AddForce(transform.up + transform.right * thrust);
+			body.AddForce(transform.up + transform.right * THRUST);
 		} else {
-			body.AddForce(transform.up - transform.right * thrust);
+			body.AddForce(transform.up - transform.right * THRUST);
 
 		}
 	}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
+	private const int SPEED = 10;
+
+	Rigidbody2D body;
+	bool leftPressed, rightPressed;
+	bool leftEdge, rightEdge;
+	int vx; // x velocity
+
 	void Start () {
-		
+		body = gameObject.GetComponent<Rigidbody2D>();
+		leftPressed = false;
+		rightPressed = false;
+		leftEdge = false;
+		rightEdge = false;
+		vx = 0;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey("left") && !leftEdge) {
+			leftPressed = true;
+			rightEdge = false;
+			vx = -1;
+		} else if (Input.GetKeyUp("left")) {
+			leftPressed = false;
+			vx = rightPressed ? 1 : 0;
+		} else if (Input.GetKey("right") & !rightEdge) {
+			rightPressed = true;
+			leftEdge = false;
+			vx = 1;
+		} else if (Input.GetKeyUp("right")) {
+			rightPressed = false;
+			vx = leftPressed ? -1 : 0;
+		}
+
+		if (vx != 0) {
+			body.position += Vector2.right * vx * SPEED;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.name == "Left Wall") {
+			leftEdge = true;
+			Vector2 p = body.position;
+			p.x = col.bounds.max.x
+				+ (body.GetComponent<Renderer>()
+						.bounds.size.x / 2);
+			body.position = p;
+			vx = 0;
+		} else if (col.gameObject.name == "Right Wall") {
+			rightEdge = true;
+			Vector2 p = body.position;
+			p.x = col.bounds.min.x
+				- (body.GetComponent<Renderer>()
+						.bounds.size.x / 2);
+			body.position = p;
+			vx = 0;
+		}
 	}
 }
